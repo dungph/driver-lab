e1b3891a9b584804f3e2f8ff223cd8ecbb1e9265
Pham Van Dung <dung18j@gmail.com>
2023-05-16 19:14:18 +0700

    dynamically create device file

--- main.c
+++ main.c
@@ -1,16 +1,52 @@
+#include "linux/export.h"
+#include "linux/stddef.h"
+#include <linux/device.h>
+#include <linux/fs.h>
 #include <linux/module.h>
 
-int init_module(void)
+struct _vchar_dr {
+  dev_t dev_num;
+  struct class *dev_class;
+  struct device *dev;
+} vchar_dr;
 
-{
-  pr_info("Init module\n");
+int init_module(void) {
+  int ret = 0;
+
+  vchar_dr.dev_num = 0;
+  ret = alloc_chrdev_region(&vchar_dr.dev_num, 0, 1, "vchar_device");
+
+  if (ret < 0) {
+    pr_info("failed to create device file\n");
+    return ret;
+  }
+  pr_info("alloc device file success\n");
+
+  vchar_dr.dev_class = class_create(THIS_MODULE, "class_vchar_dev");
+  if (vchar_dr.dev_class) {
+    pr_info("create device class success\n");
+  } else {
+    unregister_chrdev_region(vchar_dr.dev_num, 1);
+  }
+
+  vchar_dr.dev = device_create(vchar_dr.dev_class, NULL, vchar_dr.dev_num, NULL,
+                               "vchar_dev");
+  if (vchar_dr.dev) {
+    pr_info("create device success\n");
+  } else {
+    pr_info("create device failed\n");
+    class_destroy(vchar_dr.dev_class);
+    unregister_chrdev_region(vchar_dr.dev_num, 1);
+  }
+
   return 0;
 }
 
-void cleanup_module(void)
-
-{
-  pr_info("Exit module\n");
+void cleanup_module(void) {
+  device_destroy(vchar_dr.dev_class, vchar_dr.dev_num);
+  class_destroy(vchar_dr.dev_class);
+  unregister_chrdev_region(vchar_dr.dev_num, 1);
+  pr_info("Exit vchar mod\n");
 }
 
 MODULE_LICENSE("GPL");
