f46b1cd6bde65e50c76ef47c9a7fd99d43b3b29d
Pham Van Dung <dung18j@gmail.com>
2023-05-19 11:16:20 +0700

    lab1.4

--- main.c
+++ main.c
@@ -1,9 +1,30 @@
+#include "linux/moduleparam.h"
 #include <linux/module.h>
 
-int init_module(void)
+int arr[100] = {1, 2};
+int count;
 
-{
+module_param_array(arr, int, &count, 0);
+int gcd(int first_num, int second_num) {
+  while (first_num != second_num) {
+    if (first_num > second_num) {
+      first_num -= second_num;
+    } else {
+      second_num -= first_num;
+    }
+  }
+  return first_num;
+}
+int init_module(void) {
+  int i;
+  int prev = arr[0];
+
   pr_info("Init module\n");
+
+  for (i = 1; i < count; i++) {
+    prev = gcd(prev, arr[i]);
+  }
+  pr_info("gcd = %d\n", prev);
   return 0;
 }
 
