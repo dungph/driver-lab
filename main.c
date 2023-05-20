5b26f01b46e27af746eee6d169e14b563bd2126f
Pham Van Dung <dung18j@gmail.com>
2023-05-20 09:26:06 +0700

    lab1.1

--- main.c
+++ main.c
@@ -1,9 +1,34 @@
+#include "linux/moduleparam.h"
 #include <linux/module.h>
 
-int init_module(void)
+int sum(int arr[], int len) {
+  int i, ret = 0;
+  for (i = 0; i < len; i++) {
+    ret += arr[i];
+  }
+  return ret;
+}
 
-{
+int gcd(int first_num, int second_num) {
+  while (first_num != second_num) {
+    if (first_num > second_num) {
+      first_num -= second_num;
+    } else {
+      second_num -= first_num;
+    }
+  }
+  return first_num;
+}
+int init_module(void) {
+  int arr[] = {1, 2, 3, 4};
+  int len = 4;
+  int n1 = 12;
+  int n2 = 16;
+
   pr_info("Init module\n");
+
+  pr_info("sum = %d\n", sum(arr, len));
+  pr_info("gcd = %d\n", gcd(n1, n2));
   return 0;
 }
 
